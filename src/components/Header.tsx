ff9700f6235b90194467c77368b85573ff6104ee
Ana Teresa de Medeiros Negri Silva <ana.t.negri.silva@avanade.com>
2025-10-21 22:15:44 -0300

    [feat] Adding Header and Hero css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,59 @@
+import logo from "../assets/logo.svg";
+
 export default function Header() {
-  return (
-    <header className="fixed top-0 w-full bg-beige shadow-sm z-50">
-      <nav className="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
-        <img src="/logo.svg" alt="Leone Ateliê" className="h-10" />
-        <ul className="flex gap-6 text-sm font-medium">
-          <li><a href="#about">Sobre</a></li>
-          <li><a href="#catalog" >Catálogo</a></li>
-          <li><a href="#testimonials">Depoimentos</a></li>
-          <li><a href="#steps">Etapas</a></li>
-          <li><a href="#contact">Contato</a></li>
+    return (
+    <header className="w-full h-24 relative z-10 flex justify-center items-center">
+      <div className="absolute inset-0"></div>
+      
+      <nav className="absolute left-0 top-1/2 -translate-y-1/2 transform pl-8">
+        <ul className="flex items-center gap-4 bg-[#934D33] text-white px-6 py-2 rounded-full font-serif text-lg">
+          <li>
+            <a 
+              href="#" 
+              className="px-6 py-2"
+              
+            >
+              Início
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className="px-6 py-2"
+         
+            >
+              Sobre
+            </a>
+          </li>
         </ul>
-        <a
-          href="#catalog"
-          className="bg-olive text-beige px-4 py-2 rounded-md hover:bg-terracota transition"
-        >
-          Encomende
-        </a>
+      </nav>
+
+    
+      <div className="flex-shrink-0 z-200">
+        <img src={logo} alt="Leone Logo" className="h-40 w-40" />
+      </div>
+
+      <nav className="absolute right-0 top-1/2 -translate-y-1/2 transform pr-8">
+        <ul className="flex items-center gap-4 bg-[#934D33] text-white px-6 py-2 rounded-full font-serif text-lg">
+          <li>
+            <a 
+              href="#" 
+              className="px-6 py-2"
+         
+            >
+              Catálogo
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className="px-6 py-2"
+         
+            >
+              Encomende
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
